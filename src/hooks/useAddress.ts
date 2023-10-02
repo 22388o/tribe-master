5a204d66457220b035599c64fffeae7696b41cb2
nosfter <97211514+andredevjs@users.noreply.github.com>
2023-10-02 14:55:06 -0600

    use react query

--- src/hooks/useAddress.ts
+++ src/hooks/useAddress.ts
@@ -1,29 +1,23 @@
-import { useState, useEffect } from 'react';
 import { API_ENDPOINTS } from '@/data/utils/endpoints'
 import { satsToBtc} from '@/utils/utils'
+
 import { useQuery } from 'react-query';
+
+const fetchAddress = async (address: string) => {
+  const response = await fetch(`${API_ENDPOINTS.MEMPOOL_API}/address/${address}/utxo`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
 const useAddress = (address: string) => {
-  const [sats, setSats] = useState(0)
-  const [balance, setBalance] = useState(0);
+  const { data, isLoading, error } = useQuery(['address', address], () => fetchAddress(address));
 
+  // Calculate the total balance
+  const sats = data?.reduce((acc: number, utxo: any) => acc + utxo.value, 0) || 0;
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`${API_ENDPOINTS.MEMPOOL_API}/address/${address}/utxo`);
-      const utxos = await response.json();
-
-      const _sats = utxos.reduce((acc: number, utxo: any) => acc + utxo.value, 0);
-      
-      setSats(_sats);
-      setBalance(satsToBtc(_sats));
-    };
-
-    if (address) {
-        fetchData();
-    }
-  }, [address]);
-
-  return { sats, balance };
+  return { sats, balance: satsToBtc(sats) , isLoading, error };
 };
 
 export default useAddress;
