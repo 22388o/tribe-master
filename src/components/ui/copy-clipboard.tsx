202cb8aec03af7c097ef4dcc067aaaa6995ae7eb
nosfter <97211514+andredevjs@users.noreply.github.com>
2023-10-09 15:22:24 -0600

    copy address

---
+++ src/components/ui/copy-clipboard.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useCopyToClipboard } from 'react-use';
+import { Check } from '@/components/icons/check';
+import { Copy } from '@/components/icons/copy';
+import { toast} from 'react-toastify'
+import { shortenStr } from '@/utils/utils';
+type CopyClipboardProps = {
+  text: string;
+};
+
+const CopyClipboard: React.FC<CopyClipboardProps> = ({ text }) => {
+  const [copyButtonStatus, setCopyButtonStatus] = useState(false);
+  const [_, copyToClipboard] = useCopyToClipboard();
+  function handleCopyToClipboard() {
+    if (text) {
+      copyToClipboard(text);
+      toast.info(`${shortenStr(text)} copied to clipboard`)
+      setCopyButtonStatus(true);
+      setTimeout(() => {
+        setCopyButtonStatus(copyButtonStatus);
+      }, 2500);
+    }
+  }
+
+  return (
+    <div
+      title="Copy text"
+      className="mt-6 flex cursor-pointer items-center px-4 text-gray-500 transition hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
+      onClick={handleCopyToClipboard}
+    >
+      {shortenStr(text)}
+      {copyButtonStatus ? (
+        <Check className="ml-4 h-auto w-3.5 text-green-500" />
+      ) : (
+        <Copy className="ml-4 h-auto w-3.5" />
+      )}
+    </div>
+  );
+};
+
+export default CopyClipboard;
