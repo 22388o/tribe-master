53fbfa8a2797e84cc53bf5306af652fb2fb7cef2
maximotodev <maximotodev@proton.me>
2023-10-12 16:28:38 -0400

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    remotePatterns: [
+      {
+        hostname: 'image.nostr.build',
+      },
+      {
+        hostname: 'nostr.build',
+      },
+    ],
     domains: ['res.cloudinary.com'],
   },
   ...(process.env.NODE_ENV === 'production' && {
