02c687cbd2f96164a8fb88d02dd65a5a16d453b9
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-11-01 13:57:02 -0300

    Update lightning.ts

--- src/services/lightning.ts
+++ src/services/lightning.ts
@@ -1,1 +1,21 @@
+import React, { useEffect } from "react"
+import {  
+    sendPayment,
+    receivePayment,
+} from "@breeztech/react-native-breez-sdk";
 
+try {
+    const invoice = await receivePayment({
+        amountMsat: 3_000_000, 
+        description: "Invoice for 3000 sats"
+    })
+} catch (error) {
+    console.log(error)
+}
+
+const bolt11 = "..."
+try {
+    const payment = await sendPayment(bolt11, 3000)
+} catch (error) {
+    console.log(error)
+}
